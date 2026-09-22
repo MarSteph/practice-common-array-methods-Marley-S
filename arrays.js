c392f1ba852fd89ea0f834a032149d65cff384f6
Marley Stephens <marley.stephens@gmail.com>
2026-09-22 11:20:02 -0500

    Completed Task 2

--- arrays.js
+++ arrays.js
@@ -1,2 +1,5 @@
 let drinks = ["London Fog", "Miel", "Mocha"];
 let pastry = ["Scone", "Croissant", "Muffin"];
+
+console.log(`Number of drinks: ${drinks.length}`);
+console.log(`Number of pastries: ${pastry.length}`);
