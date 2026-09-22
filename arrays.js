bc10240744fcff65471984b3834a763547544a4e
Marley Stephens <marley.stephens@gmail.com>
2026-09-22 11:25:36 -0500

    Completed Task 3

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ let pastry = ["Scone", "Croissant", "Muffin"];
 
 console.log(`Number of drinks: ${drinks.length}`);
 console.log(`Number of pastries: ${pastry.length}`);
+
+console.log(`Order #1: ${drinks[0]}, ${pastry[2]}`);
+console.log(`Order #2: ${drinks[1]}, ${pastry[0]}`);
+console.log(`Order #3: ${drinks[2]}, ${pastry[1]}`);
